e471a4822af220368e735d3c451c6aeb99e0266b
Natali7772222 <n7772222@gmail.com>
2022-08-01 14:30:01 +0300

    add eslint

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 import _ from "lodash";
-console.log(_.last(['one', 'two']));
+
+console.log(_.last(["one", "two"]));
+
+const myF = (a) => {
+  return a / 2;
+};
+const funk = (a, b) => {
+  const c = a + b;
+  return c;
+};
